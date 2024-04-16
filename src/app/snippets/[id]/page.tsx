3f294924778429dfb6a4e1aa3f5bbaf33161a3d9
Hemanshu <hemanshu.mahajan@gmail.com>
2024-04-16 17:25:49 +0530

    Completed

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { notFound } from 'next/navigation';
+import Link from 'next/link';
+
+import * as actions from '@/actions';
 import { db } from '@/db';
+import { devNull } from 'os';
 
 interface SnippetShowPageProps {
   params: {
@@ -7,6 +11,8 @@ interface SnippetShowPageProps {
   };
 }
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  // await new Promise((r) => setTimeout(r, 2000));
+
   const snippet = await db.snippet.findFirst({
     where: {
       id: parseInt(props.params.id),
@@ -17,5 +23,37 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     return notFound();
   }
 
-  return <div>{snippet.title}</div>;
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
+
+  return (
+    <div>
+      <div className='flex m-4 justify-between items-center'>
+        <h1 className='text-xl font-bold'>{snippet.title}</h1>
+        <div className='flex gap-4'>
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className='p-2 border rounded'
+          >
+            Edit
+          </Link>
+          <form action={deleteSnippetAction}>
+            <button className='p-2 border rounded'>Delete</button>
+          </form>
+        </div>
+      </div>
+      <pre className='p-3 border rounded bg-gray-200 border-gray-200'>
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany();
+
+  return snippets.map((snippet) => {
+    return {
+      id: snippet.id.toString(),
+    };
+  });
 }
